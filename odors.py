3cf2bb73affb3b4a5d779409d24fe28b0c0a1d66
giacThePhantom <giacomo.fantoni@studenti.unitn.it>
2023-03-10 11:06:08 +0100

    Odor class

--- odors.py
+++ odors.py
@@ -1,53 +1,82 @@
 import numpy as np
+import random
 
 class Odor:
 
-    def __init__(self, param, num_glomeruli, homogeneous):
+    def __init__(self, param, name, num_glomeruli, homogeneous):
+        self. name = name
         self.param = param
         self.num_glomeruli = num_glomeruli
         self.homogeneous = homogeneous
         self.binding_rates = self._build_binding_rates()
         self.activation_rates = self._build_activation_rates()
 
-    def _binding_amplitude(self):
-        res = 0
-        if isinstance(params['binding_amplitude'], dict):
-            pass
+    def _compute_random_variable(self, var):
+        if isinstance(var, dict):
+            while True:
+                res = np.random.normal(var['mu'], var['sigma'])
+                if res >= var['interval'][0] and res <= var['interval'][1]:
+                    break
         else:
-            res = params['binding_amplitude']
+            res = var
         return res
+
+    def _build_binding_rate(self, binding_params, i):
+        x = i - binding_params['midpoint']
+        x = np.minimum(np.abs(x), np.abs(x + self.num_glomeruli))
+        x = np.minimum(np.abs(x), np.abs(x - self.num_glomeruli))
+        binding_profile = np.power(10, self._compute_random_variable(binding_params['amplitude']))*np.exp(-np.power(x, 2)/2*np.power(self._compute_random_variable(binding_params['sigma']), 2))
+        if binding_profile < binding_params['min_thresh']:
+            binding_profile = 0
+        return binding_profile
+
+    def _build_activation_rate(self, prev = None):
+        res = 0
+        if self.homogeneous and prev:
+            res = prev
+        else:
+            res = self._compute_random_variable(self.param['activation'])
+        return res
+
 
     def _build_binding_rates(self):
         res = []
         for i in range(self.num_glomeruli):
-            x = i - self.param['binding_midpoint']
-            x = np.minimum(np.abs(x), np.abs(x + self.num_glomeruli))
-            x = np.minimum(np.abs(x), np.abs(x - self.num_glomeruli))
-            binding_profile = np.power(10, self.binding_amplitude)*np.exp(-np.power(x, 2)/2*np.power(self.binding_sigma, 2))
-            if binding_profile < self.min_thresh:
-                binding_profile = 0
-            res.append(binding_profile)
-        return res
+            res.append(self._build_binding_rate(self.param['binding'], i))
+        return np.array(res)
 
-    def _build_activation_rates(self, homogeneous):
+    def _build_activation_rates(self):
         res = []
-        for i in range(self.num_glomeruli):
-            x = 0
-            if i > 0 and homogeneous:
-                x = res[0]
-            else:
-                print(x < self.activation_interval[0] or x > self.activation_interval[1])
-                while True:
-                    x = np.random.normal(self.activation_mu, self.activation_sigma)
-                    if x >= self.activation_interval[0] and x <= self.activation_interval[1]:
-                        break
-            res.append(x)
-        return res
+        res.append(self._build_activation_rate())
+        for i in range(1, self.num_glomeruli):
+            res.append(self._build_activation_rate(res[0]))
+        return np.array(res)
+
+    def shuffle_binding_rates(self, shuffle = None):
+        if not shuffle is None:
+            self.binding_rates = self.binding_rates[shuffle]
+        else:
+            random.shuffle(self.binding_rates)
+
+    def get_name(self):
+        return self.name
+
 
 
 
 
 
 if __name__ == "__main__":
-    temp = Odor(160, 50, 0.1, 0, 0, (0, 4), 1, 0.1, False)
-    print(temp.activation_rate)
+    import sys
+    from reading_parameters import get_parameters
+    param = get_parameters(sys.argv[1])
+    temp = Odor(param['protocols']['experiment1']['odors']['default'], 'iaa', 160, False)
+    temp1 = Odor(param['protocols']['experiment1']['odors']['default'], 'iaa', 160, False)
+    print(temp.binding_rates)
+    test = np.arange(160)
+    random.shuffle(test)
+    temp.shuffle_binding_rates(test)
+    temp1.shuffle_binding_rates(test)
+    print(temp.binding_rates)
+    print(temp.binding_rates)
+    print(temp.binding_rates == temp1.binding_rates)
