d42273f1c09436ec550a8cf596617d85435ca969
Enrico Trombetta <trombetta.enricom@protonmail.com>
2023-03-15 12:22:14 +0100

    Added pytable and hdf5 output

--- simulation.py
+++ simulation.py
@@ -12,6 +12,9 @@ from protocol import Protocol
 from first_protocol import FirstProtocol
 
 import numpy as np
+import tables
+from tqdm import tqdm
+from tqdm.contrib.logging import logging_redirect_tqdm
 
 class Simulator:
     """
@@ -49,20 +52,26 @@ class Simulator:
         self.param = param
         self.track_vars()
 
-    def _track_var(self, population: str, var_names: List[str]):
+    def _track_var(self, population: str, var_names: List[str], f: tables.File):
+        group = f.create_group(f.root, population)
         for name in var_names:
             #pop = self.model.neuron_populations[population]
             genn_pop = self.model.connected_neurons[population]
             if name == "spikes":
                 print("recording...")
                 genn_pop.spike_recording_enabled = True
+                target_cols = 2
             else:
                 # TODO: implement CUDA event buffers for those variables.
                 # For now all we can do is pulling them manually every n steps
                 
                 # Format: (v_1, v_2, ..., v_k, t) with k being the number of neurons.
+                target_cols = genn_pop.size + 1
                 self._data[population][name] = np.empty(genn_pop.size + 1)
             self.recorded_vars.setdefault(population, []).append(name)
+
+            f.create_earray(group, name, tables.Float64Atom(), (0, target_cols))
+
 
     def track_vars(self):
         """
@@ -77,35 +86,35 @@ class Simulator:
             An extra variable, "spikes", enables spike counting (ie. when V >= some threshold)
         """
 
+        self.dirpath = Path("outputs") / self.sim_name
+        self.logging_path = self.dirpath / "tracked_vars.h5"
+        self.protocol_path = self.dirpath / "protocol.pickle"
+        self.dirpath.mkdir(exist_ok=True)
+
+        with self.protocol_path.open("wb") as f:
+            pickle.dump(self.protocol, f)
+
+
         variables = [(key, self.param['tracked_variables'][key]) for key in self.param['tracked_variables']]
-        for var in variables:
-            self._track_var(*var)
+        with tables.open_file(self.logging_path, 'w') as f:
+            for var in variables:
+                self._track_var(*var, f=f)
 
     def _reset_population(self):
         self.model.clear_logs()
 
     def _stream_output(self):
-        dirpath = Path("outputs") / self.sim_name
-        logging.info(f"Saving to {dirpath}")
-        dirpath.mkdir(exist_ok=True)
+        logging.info(f"Saving to {self.dirpath}")
 
-        protocol_path = dirpath / "protocol.pickle"
-        if not protocol_path.exists():
-            with protocol_path.open("wb") as f:
-                pickle.dump(self.protocol, f)
-
-        for pop, var_dict in self._data.items():
-            for var, values in var_dict.items():
-                logpath = dirpath / f"{pop}_{var}.csv"
-                with logpath.open("a") as f:
-                    np.savetxt(f, np.column_stack(values), delimiter=",", fmt='%.4e')
+        with tables.open_file(self.logging_path, 'a') as f:
+            for pop, var_dict in self._data.items():
+                for var, values in var_dict.items():
+                    # handle both spiking events and snapshots
+                    if len(values.shape) == 1:
+                        values = values.reshape((1, -1))
+                    f.root[pop][var].append(values)
 
         self._reset_population()
-    
-    def _flush_output(self):
-        dirpath = Path("outputs") / self.sim_name
-        for file in dirpath.rglob("*"):
-            file.unlink()
     
     def _add_to_var(self, pop, var, times, series):
         # We're saving a snapshot. On that case, squeeze the output into a single row
@@ -113,7 +122,7 @@ class Simulator:
             series = series.squeeze()
             self._data[pop][var] = np.concatenate([times, series])
         else:
-            self._data[pop][var] = np.vstack([times, series])
+            self._data[pop][var] = np.column_stack([times, series])
 
     def update_target_pop(self, target_pop, current_events, events):
         for (i, event) in enumerate(current_events):
@@ -149,7 +158,6 @@ class Simulator:
         """
         model = self.model.network
         logging.info(f"Starting a simulation for the model {model.model_name} that will run for {self.protocol.simulation_time} ms")
-        self._flush_output()
 
         if not model._built:
             logging.info("Build and load")
@@ -168,50 +176,52 @@ class Simulator:
 
         target_pop = self.model.connected_neurons['or']
 
-        while model.t < self.protocol.simulation_time:
-            logging.debug(f"Time: {model.t}")
-            model.step_time()
-            self.update_target_pop(target_pop, current_events, events)
+        with logging_redirect_tqdm():
+            with tqdm(total=self.protocol.simulation_time) as pbar:
+                while model.t < self.protocol.simulation_time:
+                    logging.debug(f"Time: {model.t}")
+                    model.step_time()
+                    self.update_target_pop(target_pop, current_events, events)
 
-            if model.t > 0 and np.isclose(np.fmod(model.t, batch), 0.0):
-                print(f"Time: {model.t/1000}")
-                for pop_name, pop_vars in self.recorded_vars.items():
-                    pop = self.model.neuron_populations[pop_name]
-                    genn_pop = self.model.connected_neurons[pop_name]
-                    for var in pop_vars:
-                        if var == "spikes":
-                            if not poll_spike_readings:
-                                model.pull_recording_buffers_from_device()
-                                spike_t = genn_pop.spike_recording_data[0]
-                                spike_id = genn_pop.spike_recording_data[1]
-                            else:
-                                genn_pop.pull_current_spikes_from_device()
-                                spike_count = genn_pop.spike_count[0][0]
-                                logging.debug(f"Detected {spike_count} spike events")
-                                if spike_count > 0:
-                                    # realistically, spike_count will hardly be bigger than 1
-                                    spike_t = model.t * np.ones(spike_count)
-                                    spike_id = genn_pop.spikes[0][0][:spike_count]
+                    if model.t > 0 and np.isclose(np.fmod(model.t, batch), 0.0):
+                        for pop_name, pop_vars in self.recorded_vars.items():
+                            pop = self.model.neuron_populations[pop_name]
+                            genn_pop = self.model.connected_neurons[pop_name]
+                            for var in pop_vars:
+                                if var == "spikes":
+                                    if not poll_spike_readings:
+                                        model.pull_recording_buffers_from_device()
+                                        spike_t = genn_pop.spike_recording_data[0]
+                                        spike_id = genn_pop.spike_recording_data[1]
+                                    else:
+                                        genn_pop.pull_current_spikes_from_device()
+                                        spike_count = genn_pop.spike_count[0][0]
+                                        logging.debug(f"Detected {spike_count} spike events")
+                                        if spike_count > 0:
+                                            # realistically, spike_count will hardly be bigger than 1
+                                            spike_t = model.t * np.ones(spike_count)
+                                            spike_id = genn_pop.spikes[0][0][:spike_count]
+                                        else:
+                                            spike_t = []
+
+                                    if len(spike_t) == 0:
+                                        continue
+
+                                    self._add_to_var(pop_name, var, spike_t, spike_id)
+
+                                    logging.debug(f"pop: {pop_name}, spike_t: {spike_t}, spike_id: {spike_id}")
                                 else:
-                                    spike_t = []
+                                    genn_pop.pull_var_from_device(var)
+                                    logging.debug(f"{pop_name} -> {var}")
+                                    logging.debug(genn_pop.vars[var].view)
+                                    series = genn_pop.vars[var].view.T
+                                    times = np.array([model.t])
 
-                            if len(spike_t) == 0:
-                                continue
+                                    self._add_to_var(pop_name, var, times, series)
 
-                            self._add_to_var(pop_name, var, spike_t, spike_id)
-
-                            logging.debug(f"pop: {pop_name}, spike_t: {spike_t}, spike_id: {spike_id}")
-                        else:
-                            genn_pop.pull_var_from_device(var)
-                            logging.debug(f"{pop_name} -> {var}")
-                            logging.debug(genn_pop.vars[var].view)
-                            series = genn_pop.vars[var].view.T
-                            times = np.array([model.t])
-
-                            self._add_to_var(pop_name, var, times, series)
-
-                if save:
-                    self._stream_output()
+                        if save:
+                            self._stream_output()
+                    pbar.update(model.dT)
 
 # TODO yeet out
 class TestFirstProtocol(FirstProtocol):
