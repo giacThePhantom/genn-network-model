c68829f7b7dda38bbfcc29288b2ae6d6f5e2a20c
Enrico "erolm_a" Trombetta <trombetta.enricom@protonmail.com>
2023-04-07 22:51:45 +0200

    Still fixing annoying nix issues

--- default.nix
+++ default.nix
@@ -1,61 +1,72 @@
 with import <nixpkgs> {};
 
 let
-    cuda = cudaPackages.cudatoolkit;
-    cudaPkgs = [cuda linuxPackages.nvidia_x11 ];
+  cuda = cudaPackages.cudatoolkit;
+  cudaPkgs = [cuda linuxPackages.nvidia_x11 ];
 
-    src = fetchFromGithub {
-          url = "https://github.com/genn-team/genn";
-          sha256 = "5aa20a0f9cdba07cd899f6ff38fcdb9d2d61957e";
-        };
+  gennSrc2 = fetchFromGitHub {
+    owner = "genn-team";
+    repo = "genn";
+    rev = "5aa20a0f9cdba07cd899f6ff38fcdb9d2d61957e";
+    sha256 = "JX0pysp4GgpybeoCuCUS5uCKaiIhDl8elxMK5YBCkdc=";
+  };
 
+  gennSrc = ../genn; 
 
-    genn = stdenv.mkDerivation {
-        name = "genn";
-        version = "4.8.0";
-        src = src;
-        propagatedBuildInputs = [gnumake cuda swig];
+  genn = stdenv.mkDerivation {
+    name = "genn";
+    version = "4.8.0";
+    src = gennSrc;
+    propagatedBuildInputs = [gnumake cuda swig];
 
-        makeFlags = [ "PREFIX=$(out)"
-                      "DYNAMIC=1"
-                      "LIBRARY_DIRECTORY=$(out)/lib"];
+    makeFlags = [ "PREFIX=$(out)"
+    "DYNAMIC=1"
+    "LIBRARY_DIRECTORY=$(out)/lib"];
 
-        CUDA_PATH="${cuda}";
+    # Not needed, as we only need the static packages.
+    dontInstall = true; 
 
-        # Not needed, as we only need the static packages.
-        dontInstall = true; 
-    };
+    CUDA_PATH="${cuda}";
+  };
 
-    pythonDeps = with python310Packages; [deprecated numpy genn six psutil];
+  pythonDeps = with python310Packages; [deprecated numpy genn six psutil setuptools];
+  doCheck = false;
+
+  pygenn = python310Packages.buildPythonPackage rec {
+    pname = "pygenn";
+    version = "4.8.0";
+    src = gennSrc;
+    propagatedBuildInputs = pythonDeps ++ cudaPkgs;
+    nativeBuildInputs = [swig genn];
+
+    CUDA_PATH="${cuda}";
     doCheck = false;
+  };
 
-    pygenn = python310Packages.buildPythonPackage rec {
-        name = "pygenn";
-        version = "4.8.0";
-        src = src;
-        propagatedBuildInputs = pythonDeps ++ cudaPkgs;
-        nativeBuildInputs = [swig];
+  beegennPythonDeps = with python310Packages; [
+    matplotlib
+    numpy
+    tables
+    networkx
+    jsonschema
+    pandas
+    tqdm
+    pygenn
+    ipykernel
+    gnumake
+  ];
 
-        CUDA_PATH="${cuda}";
-    };
+  beegenn = python310Packages.buildPythonPackage {
+    pname = "beegenn";
+    version = "0.0.1";
+    src = ./.; # TODO
 
-    beegennPythonDeps = with python310Packages; [
-      matplotlib
-      numpy
-      tables
-      networkx
-      jsonschema
-      pandas
-      tqdm
-      pygenn
-    ];
+    propagatedBuildInputs = beegennPythonDeps ++ cudaPkgs;
 
-    beegenn = python310Packages.buildPythonPackage {
-      name = "beegenn";
-      version = "0.0.1"
-      src = ./.; # TODO
+    doCheck = false;
+  };
 
-      propagatedBuildInputs = beegennPythonDeps ++ cudaPkgs;
-    };
+
 in
-    beegenn
+  #beegenn
+  pygenn
