a49a57ba22645fbda9221f141a3351cdb43e1daf
giacThePhantom <giacomo.fantoni@studenti.unitn.it>
2023-09-10 00:19:25 +0200

    Updated batch script

--- launch_simulations.sh
+++ launch_simulations.sh
@@ -3,8 +3,8 @@
 conditions=( "normal" "halved" "quarter" "tenth" "hundreth")
 
 for i in "${conditions[@]}" ; do
-    code=$(qsub "genn-network-model/cluster/{$i}_synapses_poisson.pbs" | awk -F '.' '{print $1}')
-    qsub -W depend=afterok:$code "genn-network-model/cluster/{$i}_synapses_poisson_extract_feature.pbs"
-    qsub -W depend=afterok:$code "genn-network-model/cluster/{$i}_synapses_poisson_sdf.pbs"
-    qsub -W depend=afterok:$code "genn-network-model/cluster/{$i}_synapses_poisson_correlation.pbs"
+    code=$(qsub "genn-network-model/cluster/"$i"_synapses_poisson.pbs" | awk -F '.' '{print $1}')
+    qsub -W depend=afterok:$code "genn-network-model/cluster/"$i"_synapses_poisson_extract_feature.pbs"
+    qsub -W depend=afterok:$code "genn-network-model/cluster/"$i"_synapses_poisson_sdf.pbs"
+    qsub -W depend=afterok:$code "genn-network-model/cluster/"$i"_synapses_poisson_correlation.pbs"
 done
